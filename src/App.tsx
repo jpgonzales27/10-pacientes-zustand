16c77606bc9121919a7f1dcee599a1b90bea8584
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-09-25 15:52:18 -0400

    fixeado css y diseño en app

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
         <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
           Seguimiento de Pacientes <span className="text-indigo-700">Veterinaria</span>
         </h1>
-      </div>
 
-      <div className="mt-12 md:flex">
-        <PatientForm />
-        <PatientsList />
+        <div className="mt-12 md:flex">
+          <PatientForm />
+          <PatientsList />
+        </div>
       </div>
     </>
   );
